85631da05ddb08d5a0882169cd4060479f44a04f
Aleksy Grabowski <hurufu@gmail.com>
2022-07-31 22:47:57 +0200

    Even more changes in logs, because they are easy

--- main.c
+++ main.c
@@ -7,24 +7,25 @@
 #include <unistd.h>
 
 #define LOG_X(Level, Prefix, Fmt, ...) (Level > g_log_level ? (void)0 : warnx(Prefix " %s:%d\t" Fmt, __FILE__, __LINE__, ##__VA_ARGS__))
+#define LOGF_(ErrFunction, Fmt, ...) (LOG_FATAL > g_log_level ? (void)0 : ErrFunction (EXIT_FAILURE, "F %s:%d\t" Fmt, __FILE__, __LINE__, ##__VA_ARGS__))
 #define LOGDX(Fmt, ...) LOG_X(LOG_DEBUG, "D", Fmt, ##__VA_ARGS__)
 #define LOGIX(Fmt, ...) LOG_X(LOG_INFO, "I", Fmt, ##__VA_ARGS__)
 #define LOGWX(Fmt, ...) LOG_X(LOG_WARNING, "W", Fmt, ##__VA_ARGS__)
 #define LOGEX(Fmt, ...) LOG_X(LOG_ERROR, "E", Fmt, ##__VA_ARGS__)
-#define LOGFX(Fmt, ...) errx(EXIT_FAILURE, "F %s:%d\t" Fmt, __FILE__, __LINE__, ##__VA_ARGS__)
-#define LOGW(Fmt, ...) warn("W %s:%d\t" Fmt, __FILE__, __LINE__, ##__VA_ARGS__)
-#define LOGF(Fmt, ...) err(EXIT_FAILURE, "F %s:%d\t" Fmt, __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOGFX(Fmt, ...) LOGF_(errx, Fmt, ##__VA_ARGS__)
+#define LOGF(Fmt, ...) LOGF_(err, Fmt, ##__VA_ARGS__)
+#define LOGW(Fmt, ...) (LOG_WARNING > g_log_level ? (void)0 : warn("W %s:%d\t" Fmt, __FILE__, __LINE__, ##__VA_ARGS__))
 
 #define elementsof(Array) (sizeof(Array)/sizeof((Array)[0]))
 
-enum LogLevel { LOG_FATAL, LOG_ERROR, LOG_WARNING, LOG_INFO, LOG_DEBUG };
+enum LogLevel { LOG_NONE, LOG_FATAL, LOG_ERROR, LOG_WARNING, LOG_INFO, LOG_DEBUG };
 
 struct args {
     int timeout;
 };
 
 static int g_event_pipe[2] = { -1, -1 };
-static enum LogLevel g_log_level;
+static enum LogLevel g_log_level = LOG_FATAL;
 
 static const char* mode_tostring(const unsigned char mode) {
     switch (mode) {
